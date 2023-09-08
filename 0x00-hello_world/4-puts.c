5a00ccfd0c48eb6d8ad3702b5c2dfe0e521ea99b
NanaPokun <nanaseipoku25@gmail.com>
2023-09-08 04:13:02 -0700

    Printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,6 +13,6 @@ int main(void)
 	/**
 	 *The puts function starts here and ends when return value is 0
 	*/
-	puts("\"Programming is like building a multilingual puzzle  ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
