26c8666adbe8e705050fb12b1feb0cce17b689d4
NanaPokun <nanaseipoku25@gmail.com>
2023-09-08 04:54:34 -0700

    Printing with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main entry point to the program
+ *
+ * Description: The function would print the exact text provided to the output uaing printf
+ *
+ * return is always 0
+ */
+
+int main(void) 
+{
+	/*The printf function starts or is executed here*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
