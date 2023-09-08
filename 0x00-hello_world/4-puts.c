334c9619e45ae389e6bcf260b031bd9514e6f084
NanaPokun <nanaseipoku25@gmail.com>
2023-09-08 03:32:06 -0700

    Printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;    
+	/**
+	 *The program begins here
+	 the function prints a message using the puts function
+	 It returns the o
+	*/
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);    
 }
