8954be7f8320f47765f0d0b08f001c6b1b8b1016
NanaPokun <nanaseipoku25@gmail.com>
2023-09-11 01:05:21 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: The program prints the alphabet
+ *
+ * Return: Always 0
+ *
+ **/
+
+int main(void)
+{
+	/* Declaring the alpha char here and initializing*/
+	char alpha = 'a';
+
+	/* Starting the do while loop*/
+	do
+	{
+		putchar(alpha); /*prints character*/
+		alpha ++; /*updates character printed by 1*/
+	}while(alpha <= 'z'); /*checks if char meets conditions*/
+	/*Printing a newline*/
+	putchar('\n');
+
+	/*Retrun value is 0*/
+	return (0);
+}
