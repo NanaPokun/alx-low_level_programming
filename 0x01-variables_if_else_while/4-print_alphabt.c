247958aff11d705ed9dd6582be8050651ec8c011
NanaPokun <nanaseipoku25@gmail.com>
2023-09-11 02:19:56 -0700

    Ommiting e and p

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: The program prints lowercase numbers without p or e
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	
+	/*Declaring and initializing variable*/
+	char lowcase = 'a';
+
+	/*Begin the do while loop here */
+	do {
+		/*Start an if loop to check if p or e is available*/
+		if (lowcase != 'e' && lowcase != 'p')
+		{
+			putchar(lowcase); /*print the char*/
+		} lowcase++; /*update the char by 1*/
+	} while (lowcase <= 'z'); /*check if lowcase is less than or equal to z*/
+	putchar('\n');
+
+	/*Return value is 0*/
+	return (0);
+}
