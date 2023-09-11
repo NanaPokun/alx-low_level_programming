daf76e4bd48577c082552f180084af9f35a3cafe
NanaPokun <nanaseipoku25@gmail.com>
2023-09-11 04:20:09 -0700

    Numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Description:The program prints all single numbers of base 10
+ *
+ * Return: Always is 0
+ *
+ */
+
+int main(void)
+{
+	/*TVariables are declared and initialized here*/
+	int single = 0;
+	
+	/*Begin looping here*/
+	 while (single < 10)
+	{
+		printf("%d", single);
+		single++; /*updatung state*/
+	}
+	 /*Printing a new line*/
+	 printf("\n");
+
+	/*Return value is 0*/
+	return (0);
+}
