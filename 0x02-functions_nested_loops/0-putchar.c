79ba43b786bc19c11026d4588f7a7b6c0b7d6b42
NanaPokun <nanaseipoku25@gmail.com>
2023-09-12 11:14:01 -0700

    Printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Description: The program prints the _putchar
+ *
+ * Return: Always is 0
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n"); /*Print the string*/
+
+	return (0); /*return value is 0*/
+}
