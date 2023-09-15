b386cf42ef2cc5f85b34683a6a849b5af209cd13
NanaPokun <nanaseipoku25@gmail.com>
2023-09-15 01:08:58 -0700

    printing more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -Entry point to print most numbers
+ *
+ * @y: function aparameter
+ *
+ * Return: All numbers except 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int y;
+
+	/*for loop starts here*/
+	for (y = 0; y <= 9; y++)
+	{
+		if (y == 2 || y == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(y + '0');
+		}
+	}
+
+	/*print newline*/
+	_putchar('\n');
+}
