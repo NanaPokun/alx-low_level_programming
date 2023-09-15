057792cfe60326c9d5f4b11aacebf0f75055f07a
NanaPokun <nanaseipoku25@gmail.com>
2023-09-14 23:59:15 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point to the program to multiply two integers
+ *
+ * @a: function parameter
+ *
+ *@b: function parameter
+ *
+ * Return: the multiple of integers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b); /*returns the multiple of a anf b*/
+}
