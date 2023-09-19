368c6e72a921a105ee287c2ea599d03406bd1db7
NanaPokun <nanaseipoku25@gmail.com>
2023-09-19 10:22:53 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: Pointer to the string to be printed.
+*/
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
