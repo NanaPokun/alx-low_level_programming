06e12cd3a91562b6c86addcc27feac2d4386c823
NanaPokun <nanaseipoku25@gmail.com>
2023-09-19 20:06:48 -0700

    Every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int c = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && c < 224)
 	{
 		_putchar(str[i]);
 		i += 2;
+		c++;
 	}
 
 	_putchar('\n');
