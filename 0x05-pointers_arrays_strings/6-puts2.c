a414648895a9574aa7747f8bd1355447936847e8
NanaPokun <nanaseipoku25@gmail.com>
2023-09-19 19:55:43 -0700

    Every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ *
+ * @str: Pointer to the string
+ *
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while(str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
