b3d11e344c883d9a3a5ddfa73db795af754e5522
NanaPokun <nanaseipoku25@gmail.com>
2023-09-21 11:25:18 -0700

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 0 if strings are equal,positive if s1 is greater
+ * and negative if s2 is greater
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+
+	/*Compare if strings are equal in length so far*/
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	else if (s1[i] == '\0')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
