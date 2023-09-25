c444637b33b1e76633189fd9a86ba0247c3dec15
NanaPokun <nanaseipoku25@gmail.com>
2023-09-25 16:43:47 -0700

    Character string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -10,17 +11,14 @@
 */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i =0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (s + 1);
 		}
-		s++;
-	}
-	if (c == '\0')
-	{
-		return (s);  /*Return a pointer to the null terminator if c is '\0*/
 	}
 
 	return (NULL); /* Character not found*/
