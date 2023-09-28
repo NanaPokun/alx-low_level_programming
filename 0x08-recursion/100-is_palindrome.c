1e812c392f31009d0feb9c157faa2f9f1d4347e5
NanaPokun <nanaseipoku25@gmail.com>
2023-09-28 00:33:03 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,24 +11,35 @@ int is_palindrome(char *s)
 {
 	int length = _strlen(s);
 
-	/* Base case: An empty string or a string with one character is a palindrome */
 	if (length <= 1)
 	{
 		return (1);
 	}
 
-	/* Check if the first and last characters match */
-	if (s[0] != s[length - 1])
+	return (p_check(s, 0, length - 1));
+}
+
+/**
+ * p_check - Helper check to see if substring is palindrom
+ * @s: string to check
+ * @st: starting index of substring
+ * @en: ending index of substring
+ *
+ * Return: 1 if string is palindrum, 0 if not
+ *
+*/
+int p_check(char *s, int st, int en)
+{
+	if (st >= en)
+	{
+		return (1);
+	}
+	if (s[st] != s[en])
 	{
 		return (0);
 	}
 
-	/* Replace the first and last characters with null terminators */
-	s[length - 1] = '\0';
-	s++;
-
- 	/* Recursive call with the modified substring */
-	return (is_palindrome(s));
+	return (p_check(s, st + 1, en - 1));
 }
 
 /**
