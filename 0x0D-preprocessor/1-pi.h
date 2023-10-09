4b346ca94a97a96b88a3e15a157654f150ef80c1
NanaPokun <nanaseipoku25@gmail.com>
2023-10-09 03:55:35 -0700

    All headers

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+fndef _1_PI_H_
+#define _1_PI_H_
+
+#define PI 3.14159265359
+
+#endif /* _1_PI_H_ */
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar(__FILE__[0]);
+    _putchar(__FILE__[1]);
+    _putchar('\n');
+    return (0);
+}
