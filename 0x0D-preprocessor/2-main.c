482eed51dacb3f476298d3cf1b0ad442a13ab1c8
NanaPokun <nanaseipoku25@gmail.com>
2023-10-10 11:15:41 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
  * main - Entry point
  *
@@ -8,8 +6,6 @@
  */
 int main(void)
 {
-    _putchar(__FILE__[0]);
-    _putchar(__FILE__[1]);
-    _putchar('\n');
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
