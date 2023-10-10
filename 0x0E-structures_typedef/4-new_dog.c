2adebdf21d74265f63133ff5a9035f0653a13396
NanaPokun <nanaseipoku25@gmail.com>
2023-10-10 10:25:21 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,11 +5,11 @@
 
 /**
  * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
  *
- * Return: A pointer to the newly created dog, or NULL on failure.
+ * Return: Pointer to the new dog, or NULL on failure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -17,22 +17,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name_copy, *owner_copy;
 
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
+	name_copy = malloc(strlen(name) + 1);
+	owner_copy = malloc(strlen(owner) + 1);
 
 	if (name_copy == NULL || owner_copy == NULL)
 	{
 		free(name_copy); // Free any allocated memory
 		free(owner_copy);
-		free(new_dog);
+ 		free(new_dog);
 		return (NULL);
 	}
+
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
 
 	new_dog->name = name_copy;
 	new_dog->age = age;
